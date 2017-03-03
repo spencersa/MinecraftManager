2d93f5fd7b4c774ee6dcb0b2158225ee1bfed4a3
spencersa <spencersa715@gmail.com>
2017-03-02 19:38:38 -0600

    Adding server controller

--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Component({
     selector: 'home',
     template: require('./home.component.html')
 })
 export class HomeComponent {
+    public http: Http;
+    constructor(_http: Http) {
+        this.http = _http;
+    }
+
+    public startServer() {
+        debugger;
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        this.http.get('/api/Server/Start').subscribe(result => {
+        });
+    }
 }
--- Controllers/ServerController.cs
+++ Controllers/ServerController.cs
@@ -1,18 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MinecraftManager.Controllers
 {
+    [Route("api/[controller]")]
     public class ServerController : Controller
     {
 
         [HttpGet("[action]")]
-        public async void Start()
+        public void Start()
         {
+            int exitCode;
+            ProcessStartInfo processInfo;
+            Process process;
+            Console.WriteLine("start");
+            processInfo = new ProcessStartInfo
+            {
+                FileName = "C:\\Users\\Holy Shit Awesome\\Desktop\\Minecraft Server\\Run.bat",
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
 
+            process = Process.Start(processInfo);
+            process.WaitForExit();
+
+            // *** Read the streams ***
+            // Warning: This approach can lead to deadlocks, see Edit #2
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+
+            exitCode = process.ExitCode;
+
+            Console.WriteLine("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
+            Console.WriteLine("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
+            Console.WriteLine("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
+            process.Dispose();
         }
     }
 }
